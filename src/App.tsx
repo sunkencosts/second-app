710dc42f002904c039affc6f6a08348781eb2696
Ben Palmer <bpalmer@cityofkingston.ca>
2022-01-01 23:11:36 -0500

    ready to transfer to desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />}></Route>
+        <Route path='/favorites' element={<FavoritesPage />}></Route>
+        <Route path='/new-meetup' element={<NewMeetupPage />}></Route>
+      </Routes>
     </div>
   );
 }
